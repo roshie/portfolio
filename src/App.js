a960fae28cb78a45d68f64ba451136796be7430e
Roshitha <roshithaignatius21@gmail.com>
2021-09-02 22:02:15 +0530

    About page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Home from './pages/Home'; 
 import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/about" component={About}/>
+          <Route exact path="/projects" component={Projects}/>
+          <Route exact path="/contact" component={Contact}/>
         </Switch>
       </Router>
     </div>
