3140664f28b845c5c0949ff67c01eda1a135dcba
roshie <roshithaignatius21@gmail.com>
2022-08-06 20:52:18 +0530

    Add page loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,10 +10,13 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import Typewriter from "typewriter-effect";
 import { motion } from "framer-motion";
+import Loader from "./components/Loader";
+import { loadingTitle, title } from "../globalStrings";
 
 export default class Home extends React.Component {
   constructor() {
     super();
+    document.title = loadingTitle;
     this.vantaRef = React.createRef();
     this.fadeIn1 = {
       initial: {
@@ -25,44 +28,67 @@ export default class Home extends React.Component {
         opacity: 1,
         transition: {
           duration: 1.5,
+          delay: 2.5,
           ease: [0.6, -0.05, 0.1, 0.99],
         },
       },
     };
+    this.fadeIn2 = {
+      initial: {
+        opacity: 0,
+      },
+      animate: {
+        opacity: 1,
+        transition: {
+          duration: 1.5,
+          delay: 2.5,
+          ease: [0.6, -0.05, 0.1, 0.99],
+        },
+      },
+    };
+    this.state = {
+      isLoaded: false,
+    };
   }
   componentDidMount() {
-    this.vantaEffect = RINGS({
-      el: this.vantaRef.current,
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 50.0,
-      minWidth: 50.0,
-      maxHeight: 100.0,
-      maxWidth: 100.0,
-      scale: 1.0,
-      scaleMobile: 1.0,
-      backgroundColor: 0x131313,
-      color: 0x7500ff,
-    });
+    setTimeout(() => {
+      this.vantaEffect = RINGS({
+        el: this.vantaRef.current,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 50.0,
+        minWidth: 50.0,
+        maxHeight: 100.0,
+        maxWidth: 100.0,
+        scale: 1.0,
+        scaleMobile: 1.0,
+        backgroundColor: 0x131313,
+        color: 0x7500ff,
+      });
+      document.title = title;
+      this.setState({ isLoaded: true });
+    }, 1750);
   }
   componentWillUnmount() {
     if (this.vantaEffect) this.vantaEffect.destroy();
   }
   render() {
+    const { isLoaded } = this.state;
     return (
-      <Menu>
-        <div
-          ref={this.vantaRef}
-          className="d-flex flex-column align-items-md-start align-items-center"
-          style={{ height: "100vh" }}
-        >
-          <motion.div exit="exit" initial="initial" animate="animate">
-            <motion.div variants={this.fadeIn1}>
+      <>
+        <Loader className={`${!isLoaded ? "" : "d-none"}`} />
+        <motion.div exit="exit" initial="initial" animate="animate">
+          <motion.div variants={this.fadeIn2}>
+            <Menu isLoaded={isLoaded}>
               <div
-                className="text-light ml-md-5 font-mono name text-center text-md-start"
-                style={{ marginTop: "8rem", fontSize: "5rem" }}
+                ref={this.vantaRef}
+                className={`d-flex flex-column align-items-md-start align-items-center ${
+                  isLoaded ? "" : "d-none"
+                }`}
+                style={{ height: "100vh" }}
               >
+<<<<<<< HEAD
                 Roshita
               </div>
               <div className="text-light ml-md-5 font-mono fs-14 mb-2 typewriter text-center text-md-start">
@@ -99,11 +125,58 @@ export default class Home extends React.Component {
                 >
                   <FontAwesomeIcon icon={faLinkedin} />
                 </a>
+=======
+                <motion.div exit="exit" initial="initial" animate="animate">
+                  <motion.div variants={this.fadeIn1}>
+                    <div
+                      className="text-light ml-md-5 font-mono name text-center text-md-start"
+                      style={{ marginTop: "8rem", fontSize: "5rem" }}
+                    >
+                      Roshitha
+                    </div>
+                    <div className="text-light ml-md-5 font-mono fs-14 mb-2 typewriter text-center text-md-start">
+                      <Typewriter
+                        options={{
+                          strings: ["Developer. Coding enthusiast."],
+                          autoStart: true,
+                          loop: true,
+                        }}
+                      />
+                    </div>
+                    <div className="text-light ml-md-5 mt-2 text-center text-md-start">
+                      <a
+                        href="https://twitter.com/roshie_ta"
+                        className="socialIcons text-light fs-14"
+                      >
+                        <FontAwesomeIcon icon={faTwitter} />
+                      </a>
+                      <a
+                        href="https://instagram.com/"
+                        className="socialIcons text-light mx-3 fs-14"
+                      >
+                        <FontAwesomeIcon icon={faInstagram} />
+                      </a>
+                      <a
+                        href="https://github.com/roshie"
+                        className="socialIcons text-light fs-14"
+                      >
+                        <FontAwesomeIcon icon={faGithub} />
+                      </a>
+                      <a
+                        href="https://linkedin.com/in/roshitha-rajesh"
+                        className="socialIcons text-light mx-3 fs-14"
+                      >
+                        <FontAwesomeIcon icon={faLinkedin} />
+                      </a>
+                    </div>
+                  </motion.div>
+                </motion.div>
+>>>>>>> d02684c... Add page loader
               </div>
-            </motion.div>
+            </Menu>
           </motion.div>
-        </div>
-      </Menu>
+        </motion.div>
+      </>
     );
   }
 }
