f4014606edaccab8e2d55af12616fdf1e7ca2a23
roshie <roshithaignatius21@gmail.com>
2023-07-10 07:43:59 +0530

    Remove messages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -99,8 +99,7 @@ export default class Home extends React.Component {
                     <div className="text-light ml-md-5 font-mono fs-14 mb-2 typewriter text-center text-md-start">
                       <Typewriter
                         options={{
-                          strings: ["Budding Engineer.","Continuously improving to excel in engineering and technology.",
-                         "Passionate about cutting-edge tech and innovation."],
+                          strings: ["Budding Engineer."],
                           autoStart: true,
                           loop: true,
                         }}
